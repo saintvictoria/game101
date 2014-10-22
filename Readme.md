acfdedff62ca8ea240dd4dc4d64893b8c9b810e7
Victoria Nicholson <saintvictoria@gmail.com>
2014-10-22 15:02:28 -0400

    made constructs

--- Readme.md
+++ Readme.md
@@ -1,6 +1,8 @@
 This is my first game attempt!
 
-simple js game using what I know my fourth week coding
+Simple js game using what I know my fourth week coding.
+
+
 ## Yo, I'm a Readme
 
 You should probably update me so people looking at your code have an idea what it is being used for :)
--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,38 @@
 console.log('The Iron Yard Rocks');
+
+var Elephant = function (options){
+  var options = options || {};
+  this.name = options.name;
+  this.life = options.life ||'normal';
+  this.age = options.age;
+  this.color = options.color;
+};
+
+var Lion = function (options){
+  var options = options || {};
+  this.name = options.name;
+  this.age = options.age;
+  this.color = options.color;
+  this.life = options.life ||'normal';
+};
+
+var Solanga = new Elephant({
+  age: 14,
+  color: "gray",
+});
+
+var Malenda = new Elephant({
+  age: 17,
+  color: "gray"
+})
+
+var Scar = new Lion({
+  age: 14,
+  color: "yellow",
+  life: 'poor',
+})
+
+var Timba = new Lion({
+  age: 24,
+  color: "brown"
+})
