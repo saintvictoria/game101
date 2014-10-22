ed9911235cd5469c6d55efc92feb2cab08a75969
Victoria Nicholson <saintvictoria@gmail.com>
2014-10-22 15:53:16 -0400

    first attack

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -6,6 +6,10 @@ var Elephant = function (options){
   this.life = options.life ||'normal';
   this.age = options.age;
   this.color = options.color;
+  this.health = options.health;
+  this.attack = function(stomp){
+    stomp.health = (stomp.health - _.random(20, 25));
+  };
 };
 
 var Lion = function (options){
@@ -13,26 +17,37 @@ var Lion = function (options){
   this.name = options.name;
   this.age = options.age;
   this.color = options.color;
+  this.health = options.health;
+  this.attack = function(chomp){
+    chomp.health = (chomp.health - _.random(20, 25));
+  };
   this.life = options.life ||'normal';
 };
 
 var Solanga = new Elephant({
   age: 14,
   color: "gray",
+  health: 100
 });
 
 var Malenda = new Elephant({
   age: 17,
-  color: "gray"
+  color: "gray",
+  health: 100
 })
 
 var Scar = new Lion({
   age: 14,
   color: "yellow",
   life: 'poor',
+  health: 75
 })
 
 var Timba = new Lion({
   age: 24,
-  color: "brown"
+  color: "brown",
+  health: 90
 })
+
+console.log(Malenda.attack(Timba))
+console.log(Timba)
