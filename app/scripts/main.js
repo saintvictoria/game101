923cedde67a05a79618cf845abe0add99445fa4c
Victoria Nicholson <saintvictoria@gmail.com>
2014-10-25 18:02:51 -0400

    added images folder, sketched out game functions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,39 @@
-console.log('The Iron Yard Rocks');
+'use strict';
+/*
+step 1:
+chose your team from left or right side
+step 2
+pick your player
+step 3
+computer picks its player from opposite side
+step 4
+you attack
+step 5
+computer attacks
+step 6
+the first one to die loses
+step 7
+Game Over
+*/
+
+function GameController() {
+    this._turn = null;
+    this.playerChoosesLeft = function(){
+    };
+    this.playerChoosesRight = function(){
+    };
+    this.playerChoosesCombatant = function(combatant){
+    };
+    this._computerChoosesCombatant =function(){
+    };
+    this.attack = function(){
+      playerCombatant.attack();
+      computerCombatant.attack();
+    };
+    this.gameIsFinished = function(){
+    };
+};
+
 
 var Elephant = function (options){
   var options = options || {};
@@ -8,55 +43,49 @@ var Elephant = function (options){
   this.color = options.color;
   this.health = options.health;
   this.attack = function(stomped){
-    stomped.health = (stomped.health - _.random(20, 25));
+  return stomped.health  -= _.random(20, 25);
+  };
+  this.life = function(stomped) {
+    return stomped.health -= _.random(30, 40);
   };
 };
 
 var Lion = function (options){
   var options = options || {};
+  this.life = options.life;
   this.name = options.name;
   this.age = options.age;
   this.color = options.color;
   this.health = options.health;
   this.attack = function(chomped){
-    chomped.health = (chomped.health - _.random(20, 25));
+    return chomped.health -= _.random(20, 25);
   };
-  this.life = options.life ||'normal';
+  this.life = function(chomped) {
+    return chomped.health -= _.random(10,15);
+  };
 };
 
-var Solanga = new Elephant({
+var solanga = new Elephant({
   age: 14,
   color: "gray",
   health: 100
 });
 
-var Malenda = new Elephant({
+var malenda = new Elephant({
   age: 17,
   color: "gray",
   health: 100
 })
 
-var Scar = new Lion({
+var scar = new Lion({
   age: 14,
   color: "yellow",
   life: 'poor',
   health: 75
 })
 
-var Timba = new Lion({
+var timba = new Lion({
   age: 24,
   color: "brown",
   health: 90
 })
-
-console.log(Malenda.attack(Timba))
-console.log(Timba)
-
-function GameController(playersArray) {
-    this._players = playerArray;
-    this._turn = this._player[0];
-}
-GameController.prototype.isFinished = function() {
-  // check players for death
-  this._players
-}
